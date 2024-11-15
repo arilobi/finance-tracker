c0875d11399659b0cfea636be0bfa2edae1789eb
Marion Nabulobi <arinabulobi@gmail.com>
2024-11-15 10:34:26 +0300

    add finance-tracker

--- react/finance-tracker/src/Pages/ThemeContext.jsx
+++ react/finance-tracker/src/Pages/ThemeContext.jsx
@@ -7,14 +7,14 @@ export const ThemeContext = createContext();
 export const ThemeContextProvider = ({children}) => {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
-    // The prevMode is currently in light mode so when the button is triggered, it will switch to !prevMode and make the pages to be in dark mode.
+// The prevMode is currently in light mode so when the button is triggered, it will switch to !prevMode and make the pages to be in dark mode.
     const toggleTheme = () => {
         setIsDarkMode((prevMode) => !prevMode);
     }
 
-    // Rendering
+// Rendering
     return (
-        // The current value is set to dark mode and will only be triggered by using the toggleTheme function to set the children components to dark mode.
+// The current value is set to dark mode and will only be triggered by using the toggleTheme function to set the children components to dark mode.
         <ThemeContext.Provider value = {{isDarkMode, toggleTheme}}>
             {children}
         </ThemeContext.Provider>
